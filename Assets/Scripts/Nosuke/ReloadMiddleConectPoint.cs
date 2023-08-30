c1f40c6036e2fb941e41707899af27f02f22faa6
teruya <c22015@std.it-college.ac.jp>
2023-08-30 19:31:16 +0900

    完成

--- Assets/Scripts/Nosuke/ReloadMiddleConectPoint.cs
+++ Assets/Scripts/Nosuke/ReloadMiddleConectPoint.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class ReloadMiddleConectPoint : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
     public void ReloadMiddle(GameObject conectTop,GameObject conectUnder)
     {
         conectTop.transform.position = transform.GetChild(0).position;
